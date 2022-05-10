e36c77d7521edd8073c7782a2dbff800dd95b57f
solsegura <solsegura7@gmail.com>
2022-05-09 23:44:02 -0300

    Arreglo con dynamic cast

--- LP1_TP3/cINCUCAI.cpp
+++ LP1_TP3/cINCUCAI.cpp
@@ -1,5 +1,6 @@
 #include "cINCUCAI.h"
 #include "cDonante.h"
+#include "cReceptor.h"
 #define MAXDONANTES 40
 #define MAXRECEPTORES 40
 
@@ -32,12 +33,12 @@ void cINCUCAI::IngresarPaciente(cPaciente* paciente_nuevo)
 	cDonante* donante_aux = dynamic_cast<cDonante*>(paciente_nuevo);
 	if (donante_aux != NULL) {
 		//es donante
-		this->Lista_donantes->Agregar(paciente_nuevo); //como hago?
+		this->Lista_donantes->Agregar(donante_aux); 
 
 		//Luego busca los posibles receptores y devuelve una lista de posibles receptores segun los organos del donante
-		cListaReceptores* sublista = this->BuscarReceptores(paciente_nuevo->lista_organos);  //mismo problema de como aclararle que es donante
-		for (int i = 0; i < paciente_nuevo->lista_organos->getCant(); i++) {
-			cPaciente* aux_Receptor = this->ElegirReceptor(sublista, (*(paciente_nuevo->lista_organos))[i]);  //para cada organo que puede donar, busca un receptor
+		cListaReceptores* sublista = this->BuscarReceptores(donante_aux->lista_organos);  //mismo problema de como aclararle que es donante
+		for (int i = 0; i < donante_aux->lista_organos->getCant(); i++) {
+			cPaciente* aux_Receptor = this->ElegirReceptor(sublista, (*(donante_aux->lista_organos))[i]);  //para cada organo que puede donar, busca un receptor
 			cout << "el receptor (aux receptor) va a recibir el organo (paciente_nuevo->listaorganos[i]" << endl; //dejo esto aca para que se lea mas claro la onda del match
 			//recordar sacar al receptor este de la lista de receptores una vez que se haga el transplante  
 			//si no es NULL iniciar el protocolo de no se que concha
@@ -45,13 +46,14 @@ void cINCUCAI::IngresarPaciente(cPaciente* paciente_nuevo)
 	}
 
 	else {
-		this->Lista_receptores->Agregar(paciente_nuevo);  //es un receptor, lo agrego a la lista de receptores
-		cListaDonantes* aux_donantes = this->BuscarDonantes(paciente_nuevo->Organo_que_necesita); //devuelve una lista con los donantes que pueden donar el organo que necesita este receptor
+		cReceptor* receptor_aux = dynamic_cast<cReceptor*>(paciente_nuevo);
+		this->Lista_receptores->Agregar(receptor_aux);  //es un receptor, lo agrego a la lista de receptores
+		cListaDonantes* aux_donantes = this->BuscarDonantes(receptor_aux->Organo_que_necesita); //devuelve una lista con los donantes que pueden donar el organo que necesita este receptor
 
 		for (int i = 0; i < aux_donantes->getCant(); i++) {
 			cListaReceptores* sublista = this->BuscarReceptores((*aux_donantes)[i]->lista_organos);  // por cada donante veo si hay match
-			cPaciente* aux = this->ElegirReceptor(sublista, paciente_nuevo->Organo_que_necesita);  //si para el organo que necesita, hay match, aux va a ser el receptor que ingreso
-			if (aux == paciente_nuevo) {
+			cPaciente* aux = this->ElegirReceptor(sublista, receptor_aux->Organo_que_necesita);  //si para el organo que necesita, hay match, aux va a ser el receptor que ingreso
+			if (aux == receptor_aux) {
 				//el receptor encontro donante :)
 				cPaciente* Match_donante = (*aux_donantes)[i]; //me guardo al match  (match_donante le va a dar a paciente nuevo el organo que necesita
 				//iniciar el protocolo ese 
