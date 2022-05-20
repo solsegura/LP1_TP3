78904aaa66939db4480f7495f1cafd53b52155a2
fiorella <maverfiorella26@gmail.com>
2022-05-19 22:56:52 -0300

    intento de main

--- LP1_TP3/LP1_TP3.cpp
+++ LP1_TP3/LP1_TP3.cpp
@@ -11,7 +11,8 @@
 #include "cHelicoptero.h"
 #include "cCentroDeSalud.h"
 
-
+using namespace std;
+string lista_organos[9] = {"Corazon", "Higado", "Pancreas", "Huesos", "Rinion", "Pulmones", "Intestino", "Piel", "Corneas"};
 int main()
 {
 	/* ----------EXPLICACION DE ESTO--------
@@ -31,8 +32,8 @@ int main()
 	 
 	 Te dejo un sapo de regalo, lav u
 
-
-
+	 */
+/*
 	 ----------------HOLA FIO----------------------------
 	 Resolvi casi todos los problemas para que el codigo pueda correr, me parece que el unico que queda es un problema con la 
 	 sobrecarga del ==, chequeate eso (sino no se que es).
@@ -43,16 +44,12 @@ int main()
 	 Algo que se puede cambiar si tenemos la voluntad es que la fecha del donante en vez de ser de tipo tm sea cFecha, estaria buenisimo pero hay 
 	 que cambiarlo tambien en el constructor y eso.
 
-	 Si piensan algo de ContarDonaciones mandenle, EXITOOOSS. A mi lo que se me ocurre es hacerla en cCentroDeSalud que tiene toda la info pero bueno... de ahi a hacerla.
-
-
-
-
-
-
+	 Si piensan algo de ContarDonaciones mandenle, EXITOOOSS. A mi lo que se me ocurre es hacerla en 
+	 cCentroDeSalud que tiene toda la info pero bueno... de ahi a hacerla.
 */
 
 
+	/*
    printf("       |  \\ \\ | |/ /\n");
    printf("       |  |\\ `' ' /             \n");
    printf("       |  ;'      \\     / ,    \n");
@@ -73,8 +70,52 @@ int main()
    printf("       |  |  `-.        ;;;;,;' \n");
    printf("       |  |    |`-.._  ,;;;;;'  \n");
    printf("       |  |    |   | ``';;;'    \n");
-   printf("                                \n");
+   printf("                                \n");*/
+	//fechas
+	cFecha* Nacimiento1 = new cFecha(1, 5, 1971);
+	cFecha* Nacimiento2 = new cFecha(12, 3, 1998);
+	cFecha* Nacimiento3 = new cFecha(9, 4, 1960);
+	cFecha* Nacimiento4 = new cFecha(3, 4, 2001);
+	cFecha* Nacimiento5 = new cFecha(12, 11, 2002);
+	cFecha* Nacimiento6 = new cFecha(2, 12, 2004);
+	cFecha* Nacimiento7 = new cFecha(24, 1, 2001);
+
+	//vehiculos
+	cVehiculo* avion1 = new cAvion("");
+	cVehiculo* avion2 = new cAvion();
+	cVehiculo* helicoptero1 = new cHelicoptero();
+	cVehiculo* helicoptero2 = new cHelicoptero();
+	cVehiculo* ambulancia1 = new cAmbulancia();
+	cVehiculo* ambulancia2 = new cAmbulancia();
+
+	//centros 
+	cCentroDeSalud* centro1 = new cCentroDeSalud("Centro 1", "Sarmiento 1800", "Monserrat", "4469883652", "Buenos Aires", );
+	cCentroDeSalud* centro2 = new cCentroDeSalud("Centro 2","Callao 500", "Barracas", "446989652", "Cordoba");
+	cCentroDeSalud* centro3 = new cCentroDeSalud("Centro 3","Rivadavia 900", "Caballito", "445395254", "San Juan");
+	cCentroDeSalud* centro4 = new cCentroDeSalud("Centro 4","Olavarria 300", "Barracas", "4449852367", "Buenos Aires",);
+
+	//donantes
+	cPaciente* donante1 = new cDonante("", "", "22458933", "Fiorella", Nacimiento1,true, "1125253636", OP, centro1,    );
+	cPaciente* donante2 = new cDonante("", "", "33168025", "Sol", Nacimiento2, true, "1145874536", ON, centro2, );
+	cPaciente* donante3 = new cDonante("", "", "12459863", "Valentina", Nacimiento3, true, "1112345678", ABN, centro3, );
+	cPaciente* donante4 = new cDonante("", "", "13650428", "Bruno", Nacimiento4, false, "1198745632", AP, centro4, );
+	cPaciente* donante5 = new cDonante("", "", "21014258", "Juan", Nacimiento5, false, "1125896314", BP, centro1, );
+	cPaciente* donante6 = new cDonante("", "", "12543698", "Pepe", Nacimiento6, false, "1174125896", BN, centro3, );
+	cPaciente* donante7 = new cDonante("", "", "23452032", "Lorena", Nacimiento7, true, "1196301205", OP, centro2, );
+
+	//paientes
+	cPaciente* receptor1 = new cReceptor("", muy_grave, /*que poronga es patologia*/, estable, "25123456", "Julieta", Nacimiento1, true, "1112347854", OP, centro1, );
+	cPaciente* receptor2 = new cReceptor("", grave, /*que poronga es patologia*/, estable, "12456123", "Rosario", Nacimiento2, true, "1112345678", ON, centro2, );
+	cPaciente* receptor3 = new cReceptor("", leve, /*que poronga es patologia*/, estable, "22123456", "Juan", Nacimiento3, false, "1178945600", ABN, centro3, );
+	cPaciente* receptor4 = new cReceptor("", muy_leve, /*que poronga es patologia*/, inestable, "50123456", "Julio", Nacimiento4, false, "1132165488", AN, centro4, );
+	cPaciente* receptor5 = new cReceptor("", leve, /*que poronga es patologia*/, inestable, "33789456", "Belen", Nacimiento5, true, "1185296345", AP, centro3, );
+	cPaciente* receptor6 = new cReceptor("", grave, /*que poronga es patologia*/, inestable, "20789456", "Ezequiel", Nacimiento6, false, "1196325803", BN, centro1, );
+	cPaciente* receptor7 = new cReceptor("", muy_grave, /*que poronga es patologia*/, inestable, "20123456", "Florencia", Nacimiento7, true, "1185274155", ABN, centro2, );
+
+
     
+
+	//receptores
    cINCUCAI* incucai = new cINCUCAI("incucai");
 	typedef cLista<cPaciente> cListaPacientes;  //dejo esto aca para entender que onda los templates en el main
 	cListaPacientes* Lista_Pacientes = new cListaPacientes(10);
